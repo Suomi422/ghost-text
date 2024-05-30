a445dc71eb5847ea62c6b4577b7e71e9877f918d
Suomi422 <lora422@proton.me>
2024-05-30 19:23:34 +0900

    Update main.rs
    Added File encryption mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{cell::RefCell, process::exit, rc::Rc};
 use ghost_text::{OperationType, Secret};
+use native_dialog::{FileDialog, MessageDialog, MessageType};
+
 
 slint::include_modules!();
 
@@ -7,6 +9,8 @@ static DEFAULT_PASS: &str = "01234567012345670123456701234567";
 static FATAL_MESSAGE: &str = "FATAL: User interface was not found";
 static FIRST_TIME_MESSAGE: &str = "WARNING: Wasn't able to read Security Key file. If using first time please change Security Key in - Enter Key - menu. Using default Security key for now.";
 static PASSKEY_FILE_MESSAGE: &str = "WARNING: Wasn't able to save Security Key! Please make sure software has access to home folder. Using default Security key for now.";
+static PATH_MESSAGE: &str = "Warning: Wasn't able to get file path";
+static FILE_ENCRYPT_EXTENSION: &str = "enc";
 
 
 fn main() -> Result<(), slint::PlatformError> {
@@ -14,6 +18,8 @@ fn main() -> Result<(), slint::PlatformError> {
     let ui_handle = ui.as_weak();
     let ui_handle2 = ui_handle.clone();
     let ui_handle3 = ui_handle.clone();
+    let ui_handle4 = ui_handle.clone();
+    let ui_handle5 = ui_handle.clone();
 
     let ui = ui_handle.unwrap();
     ui.set_main_error_field(String::from("").into());
@@ -40,6 +46,9 @@ fn main() -> Result<(), slint::PlatformError> {
     let shared_key_for_encode = Rc::clone(&shared_key);
     let shared_key_for_decode = Rc::clone(&shared_key);
     let shared_key_for_set_key = Rc::clone(&shared_key);
+
+    let shared_key_for_encode_binary = Rc::clone(&shared_key);
+    let shared_key_for_decode_binary = Rc::clone(&shared_key);
 
 
     // Encrypt
@@ -104,6 +113,111 @@ fn main() -> Result<(), slint::PlatformError> {
                 panic!("{}", FATAL_MESSAGE);
             }
     });
+
+
+    // File encryption
+    ui.on_encrypt_file(move || {
+        let unwrapped_ui = match ui_handle4.upgrade() {
+            Some(ui) => ui,
+            None => panic!("{}", FATAL_MESSAGE)
+        };
+
+        let path = FileDialog::new()
+        .set_location("~/")
+        .set_filename("")
+        .add_filter("JPEG Image", &["jpg", "jpeg"])
+        .add_filter("PNG Image", &["png"])
+        .show_open_single_file()
+        .unwrap();
+
+        let mut path = match path {
+            Some(path) => path,
+            None => {
+                unwrapped_ui.set_cloni(PATH_MESSAGE.into());
+                return
+            },
+        };
+
+        let confirmation_dialog = MessageDialog::new()
+            .set_type(MessageType::Info)
+            .set_title("Do you want to encrypt this file?")
+            .set_text(&format!("{:#?}", path))
+            .show_confirm();
+
+        if confirmation_dialog.is_err() {
+            panic!("{}", FATAL_MESSAGE)
+            
+        }
+
+        let file_bytes = ghost_text::read_file_to_bytes(&path);
+        let mut encrypter = Secret::init(shared_key_for_encode_binary.borrow().clone(), OperationType::Encrypt);
+        let encrypted_bytes = encrypter.generate_bytes(file_bytes.unwrap());
+        path.set_extension(format!("{}.{}", path.extension().unwrap_or_default().to_str().unwrap_or(""), FILE_ENCRYPT_EXTENSION));
+        match encrypted_bytes {
+            Ok(bytes) => {
+                let writing_result = ghost_text::write_bytes_to_file(&path, &bytes);
+
+                match writing_result {
+                    Ok(_) => unwrapped_ui.set_cloni("File encrypted sucesfully".into()),
+                    Err(error) => unwrapped_ui.set_cloni(error.error_msg.into()),
+                }
+            },
+            Err(error) => unwrapped_ui.set_cloni(error.error_msg.into()),
+        }
+    });
+
+
+    // File decryption
+    ui.on_decrypt_file(move || {
+        let unwrapped_ui = match ui_handle5.upgrade() {
+            Some(ui) => ui,
+            None => panic!("{}", FATAL_MESSAGE)
+        };
+
+        let path = FileDialog::new()
+        .set_location("~/")
+        .set_filename("")
+        .add_filter("Encrypted Image", &[FILE_ENCRYPT_EXTENSION])
+        .show_open_single_file()
+        .unwrap();
+
+        let mut path = match path {
+            Some(path) => path,
+            None => {
+                unwrapped_ui.set_cloni_2(PATH_MESSAGE.into());
+                return
+            },
+        };
+
+        let confirmation_dialog = MessageDialog::new()
+            .set_type(MessageType::Info)
+            .set_title("Do you want to decrypt this file?")
+            .set_text(&format!("{:#?}", path))
+            .show_confirm();
+
+        if confirmation_dialog.is_err() {
+            panic!("{}", FATAL_MESSAGE)
+            
+        }
+
+        let file_bytes = ghost_text::read_file_to_bytes(&path);
+        let mut decrypter = Secret::init(shared_key_for_decode_binary.borrow().clone(), OperationType::Decrypt);
+        let decrypted_bytes = decrypter.generate_bytes(file_bytes.unwrap());
+
+        path.set_extension(format!("{}.{}", path.extension().unwrap_or_default().to_str().unwrap_or(""), "dec"));
+        match decrypted_bytes {
+            Ok(bytes) => {
+                let writing_result = ghost_text::write_bytes_to_file(&path, &bytes);
+
+                match writing_result {
+                    Ok(_) => unwrapped_ui.set_cloni_2("File decrypted sucesfully".into()),
+                    Err(error) => unwrapped_ui.set_cloni_2(error.error_msg.into()),
+                }
+            },
+            Err(error) => unwrapped_ui.set_cloni_2(error.error_msg.into()),
+        }
+    });
+
 
     ui.on_exit(move || exit(0));
 
